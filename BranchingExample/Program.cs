5f05ba6b9953435dbe1fe9ae2e83f0585d890264
nickahickman <nickahickman@gmail.com>
2020-10-29 10:45:38 -0400

    changed more stuff

--- BranchingExample/Program.cs
+++ BranchingExample/Program.cs
@@ -7,6 +7,7 @@ namespace BranchingExample
         static void Main(string[] args)
         {
             Console.WriteLine("Working Branch");
+            Console.WriteLine("Working Branch Intensifies");
         }
     }
 }
